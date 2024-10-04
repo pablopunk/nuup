2d6989aae919ead5daf06bc65c72fc6934e925d4
Pablo P Varela <pablo@pablopunk.com>
2024-10-04 16:31:04 +0200

    remove npm publish and format files

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # nuup
 
 <p align="center">
-  <a href="https://travis-ci.org/pablopunk/nuup"><img src="https://img.shields.io/travis/pablopunk/nuup.svg" /> </a>
-  <a href="https://codecov.io/gh/pablopunk/nuup"><img src="https://img.shields.io/codecov/c/github/pablopunk/nuup.svg" /> </a>
-  <a href="https://standardjs.com"><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg" /> </a>
-  <a href="https://github.com/pablopunk/miny"><img src="https://img.shields.io/badge/made_with-miny-1eced8.svg" /> </a>
+  <a href="https://travis-ci.org/pablopunk/nuup"><img src="https://img.shields.io/travis/pablopunk/nuup.svg" /></a>
+  <a href="https://codecov.io/gh/pablopunk/nuup"><img src="https://img.shields.io/codecov/c/github/pablopunk/nuup.svg" /></a>
+  <a href="https://standardjs.com"><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg" /></a>
+  <a href="https://github.com/pablopunk/miny"><img src="https://img.shields.io/badge/made_with-miny-1eced8.svg" /></a>
   <a href="https://www.npmjs.com/package/nuup"><img src="https://img.shields.io/npm/dt/nuup.svg" /></a>
 </p>
 
@@ -12,7 +12,7 @@
   <i>Yet another npm publish with superpowers.</i>
 </p>
 
-*nuup* takes care of changing the version in your `package.json`: **commit**, **push**, and **publish**.
+*nuup* takes care of changing the version in your `package.json`: **tag**, **commit** and **push**.
 
 ## Install
 
@@ -49,10 +49,6 @@ $ nuup 9.8.7
 * `[--version | -v]`
 
 Show *nuup* version
-
-* `[--no-npm | -n]`
-
-Bump package.json version without performing `npm publish`.
 
 
 ## License
--- index.js
+++ index.js
@@ -1,84 +1,90 @@
 #!/usr/bin/env node
 
-'use strict'
-
-const mri = require('mri')
-const isGitClean = require('is-git-clean')
-const { shell } = require('execa')
-const pkg = require('./package.json')
-const spinner = require('./lib/spinner')
-const actions = require('./lib/actions')
+const mri = require("mri");
+const isGitClean = require("is-git-clean");
+const { shell } = require("execa");
+const pkg = require("./package.json");
+const spinner = require("./lib/spinner");
+const actions = require("./lib/actions");
 
 const behindRemoteRegex = [
-  /Your branch is behind/,
-  /Your branch .* have diverged/
-]
+	/Your branch is behind/,
+	/Your branch .* have diverged/,
+];
 
-async function checkRemoteAhead (dirname) {
-  const { stdout } = await shell('git fetch && git status', { cwd: dirname })
-  const dirty = behindRemoteRegex.map(reg => reg.test(stdout))
+async function checkRemoteAhead(dirname) {
+	const { stdout } = await shell("git fetch && git status", { cwd: dirname });
+	const dirty = behindRemoteRegex.map((reg) => reg.test(stdout));
 
-  if (dirty.includes(true)) {
-    throw new Error('Remote contains changes you don\'t have yet, please `git pull` before using nuup')
-  }
+	if (dirty.includes(true)) {
+		throw new Error(
+			"Remote contains changes you don't have yet, please `git pull` before using nuup",
+		);
+	}
 }
 
-async function checkGitClean (dirname) {
-  const isClean = await isGitClean(dirname)
-  if (!isClean) {
-    throw new Error('Please commit all files before publishing')
-  }
+async function checkGitClean(dirname) {
+	const isClean = await isGitClean(dirname);
+	if (!isClean) {
+		throw new Error("Please commit all files before publishing");
+	}
 }
 
-async function performActions (commands, options) {
-  options = Object.assign({
-    dirname: process.env.PWD,
-    isDefault: false,
-    noNpm: false
-  }, options)
+async function performActions(commands, options) {
+	options = Object.assign(
+		{
+			dirname: process.env.PWD,
+			isDefault: false,
+		},
+		options,
+	);
 
-  if (options.isDefault) {
-    return actions.runDefault({ dirname: options.dirname, noNpm: options.noNpm })
-  }
+	if (options.isDefault) {
+		return actions.runDefault({ dirname: options.dirname });
+	}
 
-  const promises = []
-  for (const a of commands) {
-    promises.push(actions.run(a, { dirname: options.dirname, noNpm: options.noNpm }))
-  }
+	const promises = [];
+	for (const a of commands) {
+		promises.push(actions.run(a, { dirname: options.dirname }));
+	}
 
-  return Promise.all(promises)
+	return Promise.all(promises);
 }
 
-function parseFlags (args) {
-  const version = (args.version || args.v)
-  const noNpm = (args['no-npm'] || args.n)
+function parseFlags(args) {
+	const version = args.version || args.v;
 
-  return { version, noNpm }
+	return { version };
 }
 
-async function cli (argv) {
-  const dirname = process.env.PWD
-  const args = mri(argv)
-  const commands = args._
+async function cli(argv) {
+	const dirname = process.env.PWD;
+	const args = mri(argv);
+	const commands = args._;
 
-  if (commands.length > actions.max) {
-    throw new Error(`Number of actions (${commands.length}) is more than allowed: ${actions.max}`)
-  }
+	if (commands.length > actions.max) {
+		throw new Error(
+			`Number of actions (${commands.length}) is more than allowed: ${actions.max}`,
+		);
+	}
 
-  const flags = parseFlags(args)
+	const flags = parseFlags(args);
 
-  if (flags.version) {
-    console.log(`Version ${pkg.version}`)
-    return
-  }
+	if (flags.version) {
+		console.log(`Version ${pkg.version}`);
+		return;
+	}
 
-  spinner.start('Checking if git is clean')
-  return checkGitClean(dirname)
-    .then(checkRemoteAhead)
-    .then(() => spinner.stop('Git is clean'))
-    .then(() => performActions(commands, { isDefault: commands.length === 0, noNpm: flags.noNpm }))
-    .catch(err => spinner.fail(err.message))
+	spinner.start("Checking if git is clean");
+	return checkGitClean(dirname)
+		.then(checkRemoteAhead)
+		.then(() => spinner.stop("Git is clean"))
+		.then(() =>
+			performActions(commands, {
+				isDefault: commands.length === 0,
+			}),
+		)
+		.catch((err) => spinner.fail(err.message));
 }
 
-cli(process.argv.slice(2))
-  .catch(err => spinner.fail(err.message))
+cli(process.argv.slice(2)).catch((err) => spinner.fail(err.message));
